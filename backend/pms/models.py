6ede6d0a86d9f467d84f73b0c5dba5616ffe7ecd
Biswas Nandamuri <biswas.bin2@gmail.com>
2020-07-12 15:10:58 +1000

    feat: Added Doctor model representation
    Doctor model (extending SQLAlchemy Model class) is used as the ORM for doctors table in the database.

--- backend/pms/models.py
+++ backend/pms/models.py
@@ -1,5 +1,8 @@
+import json
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -14,3 +17,47 @@ def attach_db(app: Flask, database_path: str):
 
     db.app = app
     db.init_app(app)
+
+
+class Doctor(db.Model):
+    """
+    Doctor
+    a persistent doctor entity, extends the base SQLAlchemy Model
+    """
+    __tablename__ = "doctors"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+
+    patients = relationship('Patient',
+                            backref='doctor',
+                            lazy=True,
+                            collection_class=list,
+                            cascade="save-update, delete")
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "patients": self.patients
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return json.dumps(self.to_json())
