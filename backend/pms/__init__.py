fb5b8ba08356a17cf7000c150369938e51e01071
Biswas Nandamuri <biswas.bin2@gmail.com>
2020-07-13 18:32:44 +1000

    refactor: Added new package for API endpoints logic

--- backend/pms/__init__.py
+++ backend/pms/__init__.py
@@ -2,11 +2,9 @@ from flask import Flask
 
 from .models import attach_db
 from .utils import get_database_path
+from .api import attach_api
 
 APP = Flask(__name__)
+
 attach_db(app=APP, database_path=get_database_path(testing=False))
-
-
-@APP.route("/")
-def index():
-    return "App Works!"
+attach_api(app=APP)
