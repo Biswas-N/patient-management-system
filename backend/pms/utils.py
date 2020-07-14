a8d225f3049a5f61a275e715c65f71227934883a
Biswas Nandamuri <biswas.bin2@gmail.com>
2020-07-14 13:28:21 +1000

    refactor: Moved dummy data feeder to utils.py

--- backend/pms/utils.py
+++ backend/pms/utils.py
@@ -1,5 +1,9 @@
+import random
+
 from dotenv import load_dotenv
 import os
+
+from backend.pms.models import db, Doctor, Patient
 
 
 def get_database_path(testing: bool) -> str:
@@ -20,3 +24,37 @@ def get_database_path(testing: bool) -> str:
     else:
         # TODO: Logic for PostgreSQL connection
         pass
+
+
+def insert_dummy_data() -> None:
+    db.drop_all()
+    db.create_all()
+
+    sample_doctors = [{"name": "Dr. Biswas", "age": 25},
+                      {"name": "Dr. Batman", "age": 42},
+                      {"name": "Dr. Who", "age": 125},
+                      {"name": "Dr. Reed Richards", "age": 42}]
+
+    for doctor in sample_doctors:
+        temp_doctor = Doctor(name=doctor["name"], age=doctor["age"])
+        temp_doctor.insert()
+
+    sample_patients = [{"name": "Ben 10", "age": 10, "gender": "Male"},
+                       {"name": "Gwen Stacy", "age": 20, "gender": "Female"},
+                       {"name": "Barry Allen", "age": 23, "gender": "Male"},
+                       {"name": "Arthur Curry", "age": 52, "gender": "Male"},
+                       {"name": "Selina Kyle", "age": 30, "gender": "Female"},
+                       {"name": "Ben 10", "age": 10, "gender": "Male"},
+                       {"name": "Gwen Stacy", "age": 20, "gender": "Female"},
+                       {"name": "Barry Allen", "age": 23, "gender": "Male"},
+                       {"name": "Arthur Curry", "age": 52, "gender": "Male"},
+                       {"name": "Selina Kyle", "age": 30, "gender": "Female"},
+                       {"name": "Arthur Curry", "age": 52, "gender": "Male"},
+                       {"name": "Selina Kyle", "age": 30, "gender": "Female"}]
+
+    for patient in sample_patients:
+        temp_patient = Patient(name=patient["name"], age=patient["age"], gender=patient["gender"])
+        random_doctor = random.choice(sample_doctors)
+        temp_patient.doctor = Doctor.query.filter(Doctor.name == random_doctor["name"]).first()
+
+        temp_patient.insert()
