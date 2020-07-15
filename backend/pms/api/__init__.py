176f547f8dec9e5f5f62e2a56304533561a6a25c
Biswas Nandamuri <biswas.bin2@gmail.com>
2020-07-15 12:04:47 +1000

    feat: Doctors CRUD endpoints logic

--- backend/pms/api/__init__.py
+++ backend/pms/api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from .patients import attach_patients_api
+from .doctors import attach_doctors_api
 
 
 def attach_api(app: Flask):
@@ -8,6 +9,7 @@ def attach_api(app: Flask):
     attach_api(app)
         This function attaches Doctor and Patient CRUD API endpoints to the flask app
 
-    :param app: A Flask instance with database attached
+    app: A Flask instance with database attached
     """
     attach_patients_api(app=app)
+    attach_doctors_api(app=app)
---
+++ backend/pms/api/doctors.py
@@ -0,0 +1,164 @@
+from flask import Flask, request, abort, jsonify
+
+from ..models import Doctor
+
+ITEMS_PER_PAGE = 10
+
+
+def attach_doctors_api(app: Flask):
+    """
+    attach_doctors_api(app)
+        This function attaches Doctor CRUD API endpoints to the flask app
+
+    app: A Flask instance with database attached
+    """
+
+    @app.route("/doctors")
+    def doctors_get_all():
+        """
+        GET /doctors handler
+            Returns a JSON object holding total_doctors (count), doctors
+            (as a list) and success (boolean)
+
+            STATUS CODES: 200, 404
+            EXCEPTIONS: ResourceNotFound (404 - when tried to access pages
+                        exceeding the limit)
+        """
+        page_number = request.args.get("page", 1, type=int)
+        start = ITEMS_PER_PAGE * (page_number - 1)
+        end = start + ITEMS_PER_PAGE
+
+        doctors = Doctor.query.all()
+        formatted_doctors = [doctor.to_json() for doctor in doctors]
+
+        # Response case 1:
+        #   If no doctors record in the database,
+        #   then reply with an empty list and count of zero
+        if page_number == 1 and len(formatted_doctors) == 0:
+            return jsonify({
+                "success": True,
+                "total_doctors": 0,
+                "doctors": formatted_doctors
+            })
+        # Response case 2:
+        #   If doctors record in the database,
+        #   then reply with a json formatted doctors list and number of
+        #   doctors
+        elif page_number > 0 and start < len(formatted_doctors):
+            return jsonify({
+                "success": True,
+                "total_doctors": len(formatted_doctors),
+                "doctors": formatted_doctors[start:end]
+            })
+        # Response case 3:
+        #   If invalid case (like trying to access page outside limit),
+        #   then reply with 404
+        else:
+            abort(404)
+
+    @app.route("/doctors/<int:doctor_id>")
+    def doctor_get_single(doctor_id):
+        """
+        GET /doctors/<int:doctor_id> handler
+            Returns a JSON object holding the doctor (JSON object) and
+            success(boolean)
+
+            STATUS CODES: 200, 404
+            EXCEPTIONS: ResourceNotFound (404 - when tried to access
+                        unknown or non existing doctor)
+        """
+        doctor = Doctor.query.get_or_404(doctor_id)
+
+        return jsonify({
+            "success": True,
+            "doctor": doctor.to_json()
+        })
+
+    @app.route("/doctors", methods=["POST"])
+    def doctor_create():
+        """
+        POST /doctors handler
+            Creates a new doctor record and returns a JSON object holding
+            the new doctor data (JSON object) and success (boolean)
+
+            STATUS CODES: 200, 400
+            EXCEPTIONS: TypeError (400 - when request body has wrong data
+                        or data structure)
+        """
+        data = request.get_json()
+
+        try:
+            # Raises TypeError, when data does not include keys "name"
+            # and "age" (These two fields are non nullable)
+            new_doctor = Doctor(**data)
+            new_doctor.insert()
+
+            return jsonify({
+                "success": True,
+                "doctor": new_doctor.to_json()
+            }), 201
+        except TypeError:
+            abort(400)
+
+    @app.route("/doctors/<int:doctor_id>", methods=["PATCH"])
+    def doctor_update(doctor_id):
+        """
+        PATCH /doctors/<int:doctor_id>"
+            Updates the name or/and age of a doctor, if the key exists in the
+            incoming request body
+
+            STATUS CODES: 200, 404
+            EXCEPTIONS: ResourceNotFound (404 - when tried to update unknown
+                        or non existing doctor)
+        """
+        data = request.get_json()
+
+        doctor = Doctor.query.get_or_404(doctor_id)
+
+        if "name" in data:
+            doctor.name = data["name"]
+            doctor.update()
+
+        if "age" in data:
+            doctor.age = data["age"]
+
+        return {
+            "success": True,
+            "doctor": doctor.to_json()
+        }
+
+    @app.route("/doctors/<int:doctor_id>", methods=["DELETE"])
+    def doctor_delete(doctor_id):
+        """
+        DELETE /doctors/<int:doctor_id> handler
+            Deletes an existing doctor record and returns a JSON object with
+            deleted doctor id and success (boolean)
+
+            STATUS CODES: 200, 404
+            EXCEPTIONS: ResourceNotFound (404 - when tried to delete unknown
+                        or non existing doctor)
+        """
+        doctor = Doctor.query.get_or_404(doctor_id)
+        doctor_id_copy = doctor.id
+        doctor.delete()
+
+        return jsonify({
+            "success": True,
+            "doctor_id": doctor_id_copy
+        })
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
