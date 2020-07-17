cdc6cf21247b90bef8b175c175e08b6492635190
Biswas Nandamuri <biswas.bin2@gmail.com>
2020-07-17 23:13:40 +1000

    docs: Add documentation to auth.py

--- pms/auth.py
+++ pms/auth.py
@@ -14,12 +14,25 @@ ALGORITHMS = ["RS256"]
 
 
 class AuthError(Exception):
+    """
+    AuthError (inheriting from Exception)
+        A standardized way to communicate auth failure to endpoints
+    """
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
 
 
 def get_token_auth_header():
+    """
+    get_token_auth_header()
+        Extracts the bearer token from an auth header (if in a correct
+        format) or aborts with a 401
+
+    @Output: a jwt object (dict) containing information like permission
+        and user's email address
+    TODO: Check if the return object's skeleton is correct
+    """
     # Check if request has an Authorization header
     if 'Authorization' not in request.headers:
         raise AuthError({
@@ -46,6 +59,19 @@ def get_token_auth_header():
 
 
 def check_permissions(permission, payload):
+    """
+    check_permissions(permission, payload)
+        Checks if the user has permission to access an endpoint
+
+    @Input permission: string permission (i.e. 'post:drink')
+    @Input payload: decoded jwt payload (dict)
+
+    @Output:
+        True - If permission exists in payload['permissions']
+        Raises 400 - If permissions does not exists in payload
+        Raises 401 - If permissions list exists in payload, but given
+                     permission not in payload['permissions']
+    """
     if "permissions" not in payload:
         raise AuthError({
             "code": "invalid_claims",
@@ -61,6 +87,22 @@ def check_permissions(permission, payload):
 
 
 def verify_decode_jwt(token):
+    """
+    verify_decode_jwt(token)
+        Verifies the token authenticity by accessing the public key form
+        auth0 service
+
+    @Input token: a json web token (string)
+
+    @Output:
+        payload - (dict) containing the permissions and basic
+        info of the user
+        Raises 400 - If public key to decode the token cannot be accessed or
+            could not decode or authenticate the token
+        Raises 401 - If JWT token is invalid or
+            Token expired or
+            Incorrect API_AUDIENCE and AUTH0_DOMAIN details
+    """
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
 
@@ -123,6 +165,19 @@ def verify_decode_jwt(token):
 
 
 def requires_auth(permission=''):
+    """
+    requires_auth(permission='')
+        Decorator to wrap any endpoint and apply middleware to check
+        authentication and authorization
+
+    @Input permission: string permission (i.e. 'post:drink')
+    @Output
+        True (payload as the first argument) - If the user has required
+            permissions to access an endpoint
+        Raises 400, 401 or 403 - Following the exceptions from
+            get_token_auth_header(), verify_decode_jwt() and
+            check_permissions()
+    """
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
