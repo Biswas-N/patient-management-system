36faea6a404d82b85d3b0b3f2b793173fa8dd514
Biswas Nandamuri <biswas.bin2@gmail.com>
2020-07-21 11:55:17 +1000

    feat(client): Add basic routing and antd component library
    - Added react-router-dom and antd component library
    - Created a basic layout using antd and included react-routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import NavBar from "./components/Header";
+import Home from "./containers/HomePage/HomePage";
+import Patients from "./containers/PatientsPage/PatientsPage";
+import Doctors from "./containers/DoctorsPage/DoctorsPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Layout } from "antd";
+
+const { Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <p>Fresh App</p>
-    </div>
+    <Router>
+      <Layout className="layout">
+        <NavBar />
+        <Content style={{ padding: "0 50px" }}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/patients" component={Patients} />
+            <Route path="/doctors" component={Doctors} />
+          </Switch>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Created by Biswas Nandamuri
+        </Footer>
+      </Layout>
+    </Router>
   );
 }
 
---
+++ client/src/containers/PatientsPage/PatientsPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PatientsPage = () => {
+    return (
+        <div className="site-layout-content">Patients Page Works</div>
+    )
+};
+
+export default PatientsPage;
