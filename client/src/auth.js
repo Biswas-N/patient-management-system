3911bb4acbd53d5c66f804b34d7e79b59831d35f
Biswas Nandamuri <biswas.bin2@gmail.com>
2020-07-23 23:41:25 +1000

    feat(client): Get logged user details

--- client/src/auth.js
+++ client/src/auth.js
@@ -1,1 +1,20 @@
-export const login_url = `https://${process.env.REACT_APP_AUTH0_DOMAIN}/authorize?audience=${process.env.REACT_APP_AUDIENCE}&response_type=token&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`
+import axios from "axios";
+
+export const loginUrl = `https://${process.env.REACT_APP_AUTH0_DOMAIN}/authorize?audience=${process.env.REACT_APP_AUDIENCE}&response_type=token&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=openid%20profile`
+
+export const getCurrentUser = () => {
+        var token = localStorage.getItem("bearer_token")
+        if (token == null){
+            return null
+        }
+        else {
+            axios.get(
+                `https://${process.env.REACT_APP_AUTH0_DOMAIN}/userinfo`,
+                {
+                    headers: {"Authorization": `Bearer ${token}`}
+                }
+            ).then(res => {
+                return res.data
+            })
+        }
+}
