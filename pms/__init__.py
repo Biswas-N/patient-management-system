9fd4fc7715868a73cee196a20c6d478393ed4dcc
Biswas Nandamuri <biswas.bin2@gmail.com>
2020-07-27 18:14:38 +1000

    feat: Add CORS to flask app

--- pms/__init__.py
+++ pms/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 from pms.models import attach_db
 from pms.utils import get_database_path
 from pms.api import attach_api
 
 APP = Flask(__name__)
+CORS(app=APP)
 
 attach_db(app=APP, database_path=get_database_path(testing=False))
 attach_api(app=APP)
