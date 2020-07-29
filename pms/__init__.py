e6b68f99c86103ccbec131f30a8ead7507601349
Biswas Nandamuri <biswas.bin2@gmail.com>
2020-07-29 11:05:01 +1000

    feat: Add /insert-data endpoint to populate database

--- pms/__init__.py
+++ pms/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 from pms.models import attach_db
-from pms.utils import get_database_path
+from pms.utils import get_database_path, insert_dummy_data
 from pms.api import attach_api
 
 APP = Flask(__name__)
@@ -10,3 +10,12 @@ CORS(app=APP)
 
 attach_db(app=APP, database_path=get_database_path(testing=False))
 attach_api(app=APP)
+
+
+# Below is an utility endpoint to insert sample data in the database
+@APP.route("/insert-data")
+def insert_data():
+    insert_dummy_data()
+    return jsonify({
+        "success": True
+    })
