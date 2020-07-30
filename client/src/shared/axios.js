062056485cbe0a70cfaebf1690fad929730316df
Biswas Nandamuri <biswas.bin2@gmail.com>
2020-07-30 11:28:28 +1000

    feat(client): Add Axios object for backend API

--- client/src/shared/axios.js
+++ client/src/shared/axios.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
-const backend = axios.create({
-    baseURL: `${process.env.REACT_APP_BACKEND}/api`,
-    headers:{
-        "Authorization": `Bearer ${localStorage.getItem("bearer_token")}`
-    }
-})
+const getBackendAxios = () => {
+    return axios.create({
+        baseURL: `${process.env.REACT_APP_BACKEND}/api`,
+        headers:{
+            "Authorization": `Bearer ${localStorage.getItem("bearer_token")}`
+        }
+    })
+}
 
-export default backend;
+export default getBackendAxios;
