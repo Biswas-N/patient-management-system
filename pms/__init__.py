6134bd186d41fb2e97f1c1f40cec16aa4619ef73
Biswas Nandamuri <biswas.bin2@gmail.com>
2020-07-30 11:26:47 +1000

    feat: Add CATCH_ALL handler in flask
    The CATCH_ALL handler is responsible to forward all the react specific routes

--- pms/__init__.py
+++ pms/__init__.py
@@ -1,11 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 from pms.models import attach_db
 from pms.utils import get_database_path, insert_dummy_data
 from pms.api import attach_api
 
-APP = Flask(__name__)
+APP = Flask(
+    __name__,
+    static_folder='../client/build/static',
+    template_folder='../client/build'
+)
 CORS(app=APP)
 
 attach_db(app=APP, database_path=get_database_path(testing=False))
@@ -13,9 +17,17 @@ attach_api(app=APP)
 
 
 # Below is an utility endpoint to insert sample data in the database
-@APP.route("/insert-data")
-def insert_data():
+@APP.route("/refresh-db")
+def refresh_db():
     insert_dummy_data()
     return jsonify({
-        "success": True
+        "success": True,
+        "message": "Refreshed and populated databases"
     })
+
+
+# The home page route, which serves the react application
+@APP.route('/', defaults={'path': ''})
+@APP.route('/<path:path>')
+def index(path):
+    return render_template("index.html")
