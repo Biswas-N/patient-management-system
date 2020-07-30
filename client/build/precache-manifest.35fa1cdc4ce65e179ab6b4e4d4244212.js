c7450d7b2bf20f5770e5a7d7d9e45987740828d3
Biswas Nandamuri <biswas.bin2@gmail.com>
2020-07-30 11:24:18 +1000

    feat(client): Add production build of frontend

---
+++ client/build/precache-manifest.35fa1cdc4ce65e179ab6b4e4d4244212.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda61e5d6c5d3e71ab20f2287e8943c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d69e58c1e86221013fa9",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "2672877f6990492b3a2b",
+    "url": "/static/css/main.d26ceb4e.chunk.css"
+  },
+  {
+    "revision": "d69e58c1e86221013fa9",
+    "url": "/static/js/2.7b69350d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7b69350d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2672877f6990492b3a2b",
+    "url": "/static/js/main.5d95ad3b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
